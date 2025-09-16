54212a86d2385636cb3195909288ea8707b21510
suraj-singh-rathore <ssingh70115@gmail.com>
2025-09-16 21:20:39 +0530

    update changes

--- script.js
+++ script.js
@@ -2,3 +2,23 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+function firstPageAnimation() {
+    var tl = gsap.timeline();
+
+    tl = from("#nav", {
+        y: '-10',
+        opacity: 0,
+        duration: 1.5,
+        ease: Expo.easeInOut
+    })
+}
+
+function circleMouseFollower(){
+    window.addEventListener("mousemove", function(dets){
+        this.document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
+    })
+}
+
+circleMouseFollower();
+firstPageAnimation();
