8ac0b05d921c3ae1e9cf0a90ebca39138e5f57c7
suraj-singh-rathore <ssingh70115@gmail.com>
2025-09-17 21:34:38 +0530

    Fixed all bugs from html css

--- script.js
+++ script.js
@@ -6,10 +6,25 @@ const scroll = new LocomotiveScroll({
 function firstPageAnimation() {
     var tl = gsap.timeline();
 
-    tl = from("#nav", {
+    tl.from("#nav", {
         y: '-10',
         opacity: 0,
         duration: 1.5,
+        ease: Expo.easeInOut
+    })
+
+    .to(".boundingelem",{
+        y: 0,
+        ease: Expo.easeInOut,
+        duration: 2,
+        delay: -.1,
+        stagger: .2
+    })
+    .from("#footerhero", {
+        y: -10,
+        opacity: 0,
+        duration: 1.5,
+        delay: -.1,
         ease: Expo.easeInOut
     })
 }
