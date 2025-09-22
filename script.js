93aef00c8ab8073c8530a3a26626765036c1a27d
suraj-singh-rathore <ssingh70115@gmail.com>
2025-09-22 23:41:47 +0530

    Completed all parts of the project

--- script.js
+++ script.js
@@ -68,4 +68,30 @@ circleMouseFollower();
 firstPageAnimation();
 circleSquize();
 
+//elem rotation:
 
+document.querySelectorAll(".elem").forEach(function (elem) {
+    var rotate = 0;
+    var diffrot = 0;
+
+    elem.addEventListener("mouseleave", function(details) {
+        gsap.to(elem.querySelector("img"),{
+            opacity: 0,
+            ease: "power3",
+            duration: 0.5,
+        });
+    });
+
+    elem.addEventListener("mousemove", function(details) {
+       var diff = details.clientY - elem.getBoundingClientRect().top;
+        diffrot = details.clientX - rotate;
+        rotate = details.clientX;
+        gsap.to(elem.querySelector("img"),{
+            opacity: 1,
+            ease: "power3",
+            top: diff,
+            left: details.clientX,
+            rotate: gsap.utils.clamp(-20,20, diffrot * 0.5),
+        });
+    });
+});
